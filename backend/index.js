c653b94410ea89f659dbc0bea40bb072c2aedf16
Sebastian <sebasmarin1099@gmail.com>
2026-08-10 20:22:09 +0200

    Connect backend to MySQL with connections pool

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import pool from './config/db.js'
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('Servidor funcionando')
+})
+
+app.get('/test-db', async (req, res) => {
+    const [rows] = await pool.query('SELECT NOW() AS ahora')
+    res.json(rows)
 })
 
 const PORT = 3000
