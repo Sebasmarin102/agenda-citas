c98efcaa663c179397888a4b19dab11e1a27d121
Sebastian <sebasmarin1099@gmail.com>
2026-08-10 22:22:25 +0200

    Connect frontend to backend availability endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import pool from './config/db.js'
 import appointmentRoutes from './routes/appointmentRoutes.js'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json());
+app.use(cors());
 
 app.use('/appointments', appointmentRoutes)
 
